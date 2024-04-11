a7504298905b6e5c3c6c0d30a6c37091d5a36a39
Pkjavaqwe <prashikkamble808@gmail.com>
2024-04-12 00:53:42 +0530

    corrected belongsTo feature

---
+++ models/Product.js
@@ -0,0 +1,20 @@
+
+
+module.exports = (sequelize, DataTypes)=>{
+    const Product= sequelize.define('product',{
+        productname:{
+            type:DataTypes.STRING
+        },
+        productid:{
+            type:DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey:true
+        },
+        categoryid:{
+            type:DataTypes.INTEGER
+        },
+        
+    })
+
+    return Product
+}
